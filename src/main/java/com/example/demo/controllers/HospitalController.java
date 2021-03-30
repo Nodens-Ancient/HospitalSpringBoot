cd7f06d7f5127b5f409f94310d4380a3401ceaf2
Nodens-Ancient <60713065+Nodens-Ancient@users.noreply.github.com>
2021-03-30 13:34:28 +0300

    liquibase

--- src/main/java/com/example/demo/controllers/HospitalController.java
+++ src/main/java/com/example/demo/controllers/HospitalController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class HospitalController {
     private final HospitalService hospitalService;
